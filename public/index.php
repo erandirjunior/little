bebab0f6768f0aeb8bf06dcc8641dfed4bbeabf8
Otávio Correia <tavz.correia7@gmail.com>
2017-05-02 21:49:08 -0300

    Continuação da refatoração executada nasconfigurações

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 require_once '../vendor/autoload.php';
 require_once '../App/web.php';
 
-use Config\Init\{Bootstrap, Route};
+use Config\Init\Bootstrap;
 
-new Bootstrap(Route::getRoutes());
+new Bootstrap();
